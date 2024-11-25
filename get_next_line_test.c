44b52c46c95d3b6cdd86020f4ef478cd72b0343b
Ross Hvidsten <rhvidste@c1r3p4.hive.fi>
2024-11-25 11:20:01 +0200

    backup for get_next line

--- get_next_line_test.c
+++ get_next_line_test.c
@@ -6,7 +6,7 @@
 /*   By: rhvidste <rhvidste@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 10:04:41 by rhvidste          #+#    #+#             */
-/*   Updated: 2024/11/25 10:18:20 by rhvidste         ###   ########.fr       */
+/*   Updated: 2024/11/25 10:55:39 by rhvidste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,5 +96,21 @@ int		main()
 	printf("\n\n");
 	close(fd);
 	free(line);
-	
+
+	printf("Reading from standrard input.\n");
+	printf("---------------------------------------\n");
+	fd = 0;
+	line = get_next_line(fd);
+	printf("%s\n", line);
+	free(line);
+	while (line)
+	{
+		line = get_next_line(fd);
+		printf("%s\n", line);
+		free(line);
+	}
+	line = get_next_line(fd);
+	printf("\n\n");
+	close(fd);
+	free(line);
 }
